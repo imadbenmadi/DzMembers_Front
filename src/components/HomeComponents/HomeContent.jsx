80d28b84345342aee08b3a037741160a493aa61c
imadbenmadi <imadbenmadi891@gmail.com>
2023-11-26 15:27:47 +0100

    .

--- src/components/HomeComponents/HomeContent.jsx
+++ src/components/HomeComponents/HomeContent.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import HomeMessages from "./HomeMessages";
 import HomeLeftBar from "./homeLeftBar";
 export default function HomeContent({ UserName, IsAdmin, IsMember }) {
-    const [messages, setMessages] = useState([]);
     return (
         <div className=" pt-[65px]">
             <HomeLeftBar
